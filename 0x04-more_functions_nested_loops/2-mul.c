40ed8944cff3819603bbece7e1b517a56fc7fb0d
josephkingsleyy <josephkingsleyy@gmail.com>
2023-03-24 19:27:45 -0700

    U~

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * mul - a function that multiplies two multiplies two integers and returns the result
- * @a: firat number input
- * @b: second number input
- * Return: a * b
+ * mul - multiply 2 integers
+ * @a: int type number
+ * @b: int type number
+ * Return: result of multiplication
  */
 int mul(int a, int b)
 {
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints numbers, from 0 to 9, followed by a new line.
- *
- * Return: Always 0 (SUccess)
+ * print_numbers - print the numbers from 0 to 9
+ * Description: You can only use _putchar twice
  */
-
 void print_numbers(void)
 {
-	int n;
+	int i;
 
-	for (n = '0'; n <= '9'; n++)
+	i = 0;
+	while (i < 10)
 	{
-		_putchar(n);
+		_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
