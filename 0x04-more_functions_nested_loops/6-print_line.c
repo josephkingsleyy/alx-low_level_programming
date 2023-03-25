5d6c505629d3f49e26903eb0b47f39cea069a924
josephkingsleyy <josephkingsleyy@gmail.com>
2023-03-24 19:09:30 -0700

    U

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,17 +4,22 @@
  * print_line - a function that draws a straight line
  *
  * @n: the number of times _ should be printed
- *
+ * 
+ * Description: can only use _putchar() to print
+ * 
  * Return: Always 0 (Success)
+ *
  */
 void print_line(int n)
 {
 	int i = 0;
 
-	while (i <= 5)
+	while (i <= n)
 	{
 		_putchar("-");
 
 		i++;
 	}
+
+	_putchar('\n')
 }
