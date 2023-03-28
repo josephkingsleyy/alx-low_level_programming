53637f0db13f29c5e46c530814ff7745a2713b9e
josephkingsleyy <josephkingsleyy@gmail.com>
2023-03-27 19:11:56 -0700

    Update on repo

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,8 +4,6 @@
  * print_rev - a function that prints a string in reverse followed by a new line
  *
  * @s: Variable holding the string to be printed in reverse
- *
- * Return: Always 0 (Success)
  */
 void print_rev(char *s)
 {
