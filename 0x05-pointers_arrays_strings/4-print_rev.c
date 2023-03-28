142a278ab7d0dc0a8158271ba91e6797aad0f322
josephkingsleyy <josephkingsleyy@gmail.com>
2023-03-27 19:10:03 -0700

    Update on repo

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,13 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int index;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	for (index = index - 1; s[index] != '\0'; index--)
 	{
-		for (i = i - 1; s[i] != '\0'; i++)
-		{
-			_putchar(s[i]);
-		}
-		_putchar('\n');
+		_putchar(s[index]);
 	}
+	_putchar('\n');
 }
