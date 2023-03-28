71cbd536fd21fda949027e3a4b64792b2026a7e8
josephkingsleyy <josephkingsleyy@gmail.com>
2023-03-27 21:07:11 -0700

    Update on repo

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,6 @@
 
 /**
  * print_array - function that prints n elements of an array of integers, followed by a new line
- *
  * @a: This is the input array
  * @n: This is the lenght of the array
  */
