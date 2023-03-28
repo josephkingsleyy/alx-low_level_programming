3f35033e13e78d6972e3961847a55dc3c1d3fcea
josephkingsleyy <josephkingsleyy@gmail.com>
2023-03-27 20:07:31 -0700

    Update on repo

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,24 +8,17 @@
 
 void rev_string(char *s)
 {
-	int a, len;
+	char ch, *p, *q;
 
-	char *begin, *end = s;
-
-	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
+	for (q = s; *q != '\0'; ++q)
+		;
+	if (q > s)
+		--q;
+	for (p = s; p < q; ++p, --q)
 	{
-		end++;
+		ch = *p;
+		*p = *q;
+		*q = ch;
 	}
-	len = a + 1;
-	begin = s;
-	for (a = 0; a < len / 2; a++)
-	{
-		char x;
-		x = *end;
-		*end = *begin;
-		*begin = x;
-		begin++;
-		end--;
-	}
-	end[len + 1] = '\0';
+	return (s);
 }
