7ab72812ddf57ca55f32e133197751479045955a
josephkingsleyy <josephkingsleyy@gmail.com>
2023-03-27 18:14:17 -0700

    Update on repo

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,7 +4,6 @@
  * _puts - a function that prints a string to stdout followed by a new line
  *
  * @str: Variable holding string to be printed
- * Return: Always 0 (Success)
  */
 void _puts(char *str)
 {
