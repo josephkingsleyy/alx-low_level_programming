7ca8c02a244f0ac3c1e15dcda2b4bfb603b11493
josephkingsleyy <josephkingsleyy@gmail.com>
2023-03-30 20:08:15 -0700

    Update on repo

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,13 @@
 
 char *_strncat(char *dest, char *src, int n) 
 {
-	    int dest_len = 0;
-  	
-	    while (dest[dest_len] != '\0') 
-	    {
-		    dest_len++;
-	    }
-    
-	    int i;
+	int i, c;
 
-	    for (i = 0; i < n && src[i] != '\0'; i++) 
-	    {
-		    dest[dest_len + i] = src[i];
-	    }
-    
-	    dest[dest_len + i] = '\0';
-	    return dest;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+	{
+		dest[i] = src[c];
+	}
+	return (dest);
 }
-
