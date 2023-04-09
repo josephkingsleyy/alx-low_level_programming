843cb0d8cb6b8fdbbd5990171266f3ae3cbc5664
josephkingsleyy <josephkingsleyy@gmail.com>
2023-04-09 11:15:40 -0700

    Update on Repo

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * A program that prints its name followed by a new line
+ *
+ * Return: 0 on Success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return(0);
+}
