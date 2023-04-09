328c0490d294b8cc2a7dafbdecf5cfbd230e644f
josephkingsleyy <josephkingsleyy@gmail.com>
2023-04-09 11:27:37 -0700

    Update on repo

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,11 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * A program that prints its name followed by a new line
- *
+ *main - prints its name followed by a new line
+ *@argc: number of arguments typed
+ *@argv: array pointer to arguments
  * Return: 0 on Success
  */
 int main(int argc, char *argv[])
